dd152a663981bf840824629c87fa764cc266f501
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-05-25 15:47:18 +0200

    merge function

--- merge.js
+++ merge.js
@@ -1,5 +1,12 @@
 import {print, exercise} from "./utils.js"
 
 exercise("Merge", function() {
-  print("Diese Aufgabe muss noch gemerged werden!")
+  let sum = 0
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+  }
+
+  print("Summe: " + sum)
 })
